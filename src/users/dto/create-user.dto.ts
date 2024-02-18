04d2485dcdb0daea95314e8e3aa87e43719084c9
Dalairajesh <dalairajesh22@gmail.com>
2024-02-18 10:52:24 +0530

    resolve some error in validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsNotEmpty } from "class-validator"
+
 export class CreateUserDto {
+    @IsNotEmpty()
     username:string
+    @IsNotEmpty()
     age:number
+    @IsNotEmpty()
     hobbies:string[]
 }
